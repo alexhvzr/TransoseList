e2592470e2aeee44d81ff6e10f79d6a3b3addc11
Alex Hayden van Zuiden-Rylander <alex@alexs-mbp.lan>
2019-03-10 20:08:28 -0700

    First Commit

---
+++ cmake-build-debug/CMakeFiles/TransposeList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TransposeList.dir/main.cpp.o"
+  "TransposeList.pdb"
+  "TransposeList"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TransposeList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
